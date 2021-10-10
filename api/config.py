632b72241de45c843975721d6d20d462466bcb38
Eliran-b <eliran646@gmail.com>
2021-10-10 10:53:37 +0300

    modify imports

--- api/config.py
+++ api/config.py
@@ -1,17 +1,16 @@
-from api import secrets
-import os
 
 
 class Config:
+     from api import secrets
      #connect db
-    test_conn = 'postgresql://{0}:{1}@{2}/{3}'.format(secrets.dbuser, secrets.dbpass, secrets.dbhost, secrets.dbname)
-    prod_conn = secrets.prod_uri
+     test_conn = 'postgresql://{0}:{1}@{2}/{3}'.format(secrets.dbuser, secrets.dbpass, secrets.dbhost, secrets.dbname)
+     prod_conn = secrets.prod_uri
 
-    SECRET_KEY = secrets.SuperSecretKey
-    SQLALCHEMY_DATABASE_URI = prod_conn
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+     SECRET_KEY = secrets.SuperSecretKey
+     SQLALCHEMY_DATABASE_URI = prod_conn
+     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     #handle token and auth
-    JWT_SECRET_KEY = secrets.jwt_key
-    JWT_ACCESS_TOKEN_EXPIRES = secrets.jwt_time
+     JWT_SECRET_KEY = secrets.jwt_key
+     JWT_ACCESS_TOKEN_EXPIRES = secrets.jwt_time
    
